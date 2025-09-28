34dab5bef9626f40c41669aa2cbaadb69de9702f
zetaeffe <fraz666@blu.it>
2025-09-28 19:15:41 +0200

    writer

--- web-app/routes/api/strat.tsx
+++ web-app/routes/api/strat.tsx
@@ -4,6 +4,7 @@ export const handler = define.handlers({
   async POST(ctx) {
     const req = await ctx.req.json()
     // console.log(req);
-    return new Response();
+    await Deno.writeTextFile( `${Deno.cwd()}/static/lines.log`, JSON.stringify(req.lines));
+    return new Response("ok", { status: 200 })
   },
 });
